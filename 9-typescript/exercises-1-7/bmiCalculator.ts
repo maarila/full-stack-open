a4fbee8025cf03e8267c0f18b2d58c55663bbdc7
maarila <maarila@gmail.com>
2023-09-12 12:45:30 +0300

    Return exercise 9.1

--- 9-typescript/exercises-1-7/bmiCalculator.ts
+++ 9-typescript/exercises-1-7/bmiCalculator.ts
@@ -1,5 +1,22 @@
-const calculateBmi(weight: number, height: number) => {
-    return 0;
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight / (height / 100) ** 2;
+  if (bmi < 16) {
+    return 'Underweight (severe thinness)';
+  } else if (bmi < 17) {
+    return 'Underweight (moderate thinness)';
+  } else if (bmi < 18.5) {
+    return 'Underweight (mild thinness)';
+  } else if (bmi < 25) {
+    return 'Normal (healthy weight)';
+  } else if (bmi < 30) {
+    return 'Overweight (Pre-obese)';
+  } else if (bmi < 35) {
+    return 'Obese (Class I)';
+  } else if (bmi < 40) {
+    return 'Obese (Class II)';
+  } else {
+    return 'Obese (Class III)';
+  }
 }
 
 console.log(calculateBmi(180, 74));
