515e366273a4062b49a1e6d97e66c6905ff94a5b
Mika Äärilä <maarila@gmail.com>
2024-04-27 18:34:43 +0300

    Return exercise 9.3

--- 9-typescript/exercises-1-7/bmiCalculator.ts
+++ 9-typescript/exercises-1-7/bmiCalculator.ts
@@ -19,4 +19,24 @@ const calculateBmi = (height: number, weight: number) => {
   }
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  if (process.argv.length < 4) throw new Error('Not enough arguments');
+  if (process.argv.length > 4) throw new Error('Too many arguments');
+
+  const firstNumber: number = Number(process.argv[2]);
+  const secondNumber: number = Number(process.argv[3]);
+
+  const isFirstInputNaN = isNaN(firstNumber);
+  const isSecondInputNaN = isNaN(secondNumber);
+
+  if (isFirstInputNaN || isSecondInputNaN)
+    throw new Error('Something other than number in input');
+
+  console.log(calculateBmi(firstNumber, secondNumber));
+} catch (error: unknown) {
+  let errorMessage = 'Uh oh, something went wrong.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- 9-typescript/exercises-1-7/exerciseCalculator.ts
+++ 9-typescript/exercises-1-7/exerciseCalculator.ts
@@ -38,4 +38,24 @@ const calculateExercises = (exercises: number[], targetHours: number) => {
   return result;
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  if (process.argv.length < 4) throw new Error('Not enough arguments');
+
+  const exercises = process.argv
+    .slice(2)
+    .map((exerciseHours) => Number(exerciseHours));
+
+  const includesNaN = exercises.some((exerciseHours) => isNaN(exerciseHours));
+
+  if (includesNaN) throw new Error('Something other than number in input');
+
+  const targetHours: number = exercises[0];
+
+  console.log(calculateExercises(exercises.slice(1), targetHours));
+} catch (error: unknown) {
+  let errorMessage = 'Uh oh, something went wrong.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
