4acd6571429b43919626f532d3728c800442b92d
Mika Äärilä <maarila@gmail.com>
2024-04-27 19:20:26 +0300

    Return exercise 9.5

--- 9-typescript/exercises-1-7/index.ts
+++ 9-typescript/exercises-1-7/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+import calculateBmi from './calculateBmi';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  const isValidInput = !isNaN(height) && !isNaN(weight);
+
+  if (!isValidInput) {
+    res.status(400).send({ error: 'malformatted parameters' });
+  } else {
+    const calculationResponse = calculateBmi(height, weight);
+    res.send({ weight: weight, height: height, bmi: calculationResponse });
+  }
 });
 
 const PORT = 3003;
