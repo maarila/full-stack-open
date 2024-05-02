6d8d46e39c701fcc859513bcff0dba5c3be59e83
Mika Äärilä <maarila@gmail.com>
2024-05-02 08:34:40 +0300

    Return exercise 9.9

--- 9-typescript/exercises-8-13/index.ts
+++ 9-typescript/exercises-8-13/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
-const app = express();
-app.use(express.json());
+const cors = require('cors');
 
-const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
